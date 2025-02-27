c1ee0c8312fc01c5f4d3c6b3e32f81b33dd3b620
aman123 <amanuelbizuayehu123@gmail.com>
2025-02-27 15:07:51 +0300

    fetching for separate data and EDA

--- src/data_fetchnig.py
+++ src/data_fetchnig.py
@@ -5,8 +5,8 @@ tickers = ['TSLA', 'BND', 'SPY']
 
 start_date = '2015-01-01'
 end_date = '2025-01-31'
+for ticker in tickers:
+    data = yf.download(ticker,start_date,end_date)
+    data.columns = ["{}_{}".format(col[0], col[1]) for col in data.columns]
 
-data = yf.download(tickers,start_date,end_date)
-data.columns = ["{}_{}".format(col[0], col[1]) for col in data.columns]
-
-data.to_csv("financial_raw_data.csv")
+    data.to_csv(f"{ticker}_raw_data.csv")
